1e04d18dadb04a1e86313c26777b5b155aa5743d
Darshan B M <90410496+Darshan-B-M@users.noreply.github.com>
2024-05-28 20:47:08 +0530

    Update Text_to_Speech.py

--- Text_to_Speech.py
+++ Text_to_Speech.py
@@ -1,16 +1,16 @@
 import os
 
-print("-------Welcome to Darshan's text to speech Bot--------")
-print("--->To Exit enter 'bye'")
+print("------------------------------welcome to Darshan's text to speech--------------------------------")
+print("---> Enter 'bye' to exit")
 
 while True:
-        str=input("Enter the text")
-        
-        command =f"  PowerShell -Command \"Add-Type –AssemblyName System.Speech;    
-        (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{str}');\""    #ms speech command line from stack overflow
-        
-        os.system(command)
+    str=input("Enter the Text to Speech")
 
-        if str=="bye":
-            break
-print("Thank you")
+    command = (f"  PowerShell -Command \"Add-Type –AssemblyName System.Speech; "
+               f"(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{str}');\"")  # ms speech command line from stack overflow
+
+    os.system(command)
+
+    if str=="bye":
+        break
+print("Thank You")
